8cf2e737e8d0ebf3993225c967445dab05ff3541
absanyal <absanyal.ABS@gmail.com>
2023-10-26 13:56:27 -0400

    scale changed

--- endtoend.py
+++ endtoend.py
@@ -6,6 +6,7 @@ x1, y1, z1, x2, y2, z2 = np.loadtxt("endtoend.txt", unpack=True)
 lines = len(x1)
 
 ete_distance = []
+displacement = []
 time_step = []
 
 averagingsq_list = []
@@ -25,8 +26,15 @@ for i in range(lines):
         pow(y1[i] - y2[i], 2) +
         pow(z1[i] - z2[i], 2)
     )
+    
+    displacement_val = np.sqrt(
+        pow(x1[i] - x1[0], 2) +
+        pow(y1[i] - y1[0], 2) +
+        pow(z1[i] - z1[0], 2)
+    )
 
     ete_distance.append(distance)
+    displacement.append(displacement_val)
     time_step.append(i)
 
     if (i >= 0.9 * lines):
@@ -45,6 +53,13 @@ print("RMS end-to-end distance =", rms_val, "+/-", stddevdist)
 
 
 plt.plot(time_step, ete_distance, label="Raw data")
-# plt.plot(time_step, np.convolve(ete_distance, window(50), 'same'),
-#          label="time averaged")
+plt.plot(time_step, np.convolve(ete_distance, window(100), 'same'),
+         lw='2.5', color='red', label="Smooth line")
+plt.legend()
+plt.ylim((105, 125))
+plt.show()
+
+plt.plot(time_step, displacement)
+plt.xscale('log')
+plt.yscale('log')
 plt.show()
